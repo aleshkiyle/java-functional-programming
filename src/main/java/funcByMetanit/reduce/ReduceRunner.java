76a36bd116935d3b0e00974072a6e57a6f4f11f1
aleshkiyle <aleksey.rodin62@gmail.com>
2022-08-14 18:31:27 +0300

    create class with realization reduce method stream api

---
+++ src/main/java/funcByMetanit/reduce/ReduceRunner.java
@@ -0,0 +1,17 @@
+package funcByMetanit.reduce;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class ReduceRunner {
+
+    public static void main(String[] args) {
+        Stream<Integer> numbers = Stream.of(1, 2, 3, 4, 5);
+        Optional<Integer> number = numbers.reduce((x, y) -> x * y);
+        number.ifPresent(System.out::println);
+
+        Stream<String> wordsStream = Stream.of("mama", "mila", "ramu");
+        Optional<String> word = wordsStream.reduce((x, y) -> x + " " + y);
+        word.ifPresent(System.out::println);
+    }
+}
